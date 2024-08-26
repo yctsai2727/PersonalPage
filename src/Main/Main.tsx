58c56a64349c94ef24856837cae68f072e7c372f
yctsai2727 <yctsai@connect.ust.hk>
2024-08-26 11:51:34 +0900

    Revert "26/8/2024"
    This reverts commit 2c66236355c4a7bdc2972cf680ab8736ca6f2882.

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -10,58 +10,64 @@ import ContactPageOutlinedIcon from '@mui/icons-material/ContactPageOutlined';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import data from '../job_exp.json'
-const style = {
-    coverimagebox: {
+const style={
+    coverimagebox:{
         backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${CoverImage})`,
         backgroundSize: '100% 16rem',
         backgroundRepeat: 'no-repeat',
-        height: "16rem"
+        height:"16rem"        
     }
 }
-export default function Main(props: { child: ReactNode }) {
-    return (
-        <Box width="100%" minHeight="100vh" sx={{ backgroundColor: "#F2F6F8" }}>
-            <Box display='flex' justifyContent="space-evenly" paddingBottom="20pt">
-                <Box position="relative" height="100%" width="100%" sx={style.coverimagebox}>
-                    <Box width="100%" position="relative" top="35%" display="flex" alignItems="center" flexDirection="column">
-                        <Typography fontFamily="Bradley Hand" color="white">Yun Chen Tsai</Typography>
-                        <Typography fontFamily="Bradley Hand" color="white">Theoretical Computer Scientist & Mathematician</Typography>
-                        <Typography fontFamily="Bradley Hand" color="white">Ph.D. & Research Assistant@NII</Typography>
-                        <Box display="flex" color="white">
-                            <IconButton aria-label="Email" color="inherit" href="mailto:yctsai@connect.ust.hk">
-                                <EmailIcon />
-                            </IconButton>
-                            <IconButton aria-label="GitHub" color="inherit" href="https://github.com/yctsai2727">
-                                <GitHubIcon />
-                            </IconButton>
-                            <IconButton aria-label="GitHub" color="inherit" href="https://github.com/yctsai2727/resume/blob/main/resume.pdf">
-                                <ContactPageOutlinedIcon />
-                            </IconButton>
-                        </Box>
-                    </Box>
-                    <Box height="100%" width="100%">
-                        <Box height="100%" display="flex" justifyContent="space-evenly" alignItems="center" flexDirection="column">
-
-                            <Box mt={"8rem"} >
-                                <Avatar sx={{ width: '180px', height: '180px', backgroundColor: "#FFFFFF" }}>
-                                    <Avatar
-                                        alt="Tasi Yun Chen"
-                                        src={Myimage}
-                                        sx={{ width: '170px', height: '170px' }}
-                                    />
-                                </Avatar>
-                            </Box>
-                        </Box>
-                    </Box>
-                </Box>
-            </Box>
-            <Box mt="90pt" display="flex" width="100%" justifyContent="space-evenly">
-                <Box width="80%">
-                    {props.child}
-
-                </Box>
-
-            </Box>
+export default function Main (props:{child:ReactNode}) {
+    return(
+    <Box width="100%" minHeight="100vh" sx={{backgroundColor:"#F2F6F8"}}>
+    <Box display='flex' justifyContent="space-evenly">
+    <Box position="relative" height="100%" width="100%" sx={style.coverimagebox}>
+        <Box width="100%" position="relative" top="35%" display="flex" alignItems="center" flexDirection="column">  
+        <Typography fontFamily="Bradley Hand" color="white">Yun Chen Tsai</Typography>
+        <Typography fontFamily="Bradley Hand" color="white">Theoretical Computer Scientist & Mathematician</Typography>
+        <Typography fontFamily="Bradley Hand" color="white">Research Assistant@HKUST</Typography>
+        <Box display="flex" color="white">
+        <IconButton aria-label="Email" color="inherit" href="mailto:yctsai@connect.ust.hk">
+             <EmailIcon />
+        </IconButton>
+        <IconButton aria-label="GitHub" color="inherit" href="https://github.com/yctsai2727"> 
+             <GitHubIcon />
+        </IconButton>
+        <IconButton aria-label="GitHub" color="inherit" href="https://github.com/yctsai2727/resume/blob/main/resume.pdf"> 
+             <ContactPageOutlinedIcon />
+        </IconButton>
         </Box>
+        {/* <IconButton aria-label="Instagram" color="inherit" href="https://www.instagram.com/samlcw0203/">
+             <InstagramIcon />
+        </IconButton>
+        <IconButton aria-label="Facebook" color="inherit" href="https://www.facebook.com/samleung26070728/">
+             <FacebookIcon />
+        </IconButton> */}
+        </Box>
+        <Box height="100%" width="100%">
+        <Box  height="100%"  display="flex" justifyContent="space-evenly" alignItems="center" flexDirection="column">
+
+        <Box  mt={"8rem"} >
+        <Avatar sx={{ width: '180px', height: '180px' , backgroundColor:"#FFFFFF"}}>
+        <Avatar
+              alt="Tasi Yun Chen"
+              src={Myimage}
+              sx={{ width: '170px', height: '170px' }}
+            />
+        </Avatar>
+        </Box>
+        </Box>
+        </Box>
+    </Box>
+    </Box>
+    <Box mt="90pt" display="flex" width="100%" justifyContent="space-evenly">
+        <Box width="80%">
+        {props.child}
+
+        </Box>
+        
+    </Box>
+    </Box>
     )
 }
