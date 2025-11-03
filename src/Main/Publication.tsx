83b88e31ac054e9b1f3ae6bbc9d5b267dd11d9f7
yctsai2727 <yctsai@connect.ust.hk>
2025-11-03 23:47:53 +0900

    Added link to DOI and open access

--- src/Main/Publication.tsx
+++ src/Main/Publication.tsx
@@ -75,7 +75,7 @@ class PublicationList extends React.Component{
                     {item.entryTags.title}
                 </Typography>
                 <Typography variant="body2" color="text.secondary" display="flex">
-                    {item.entryTags.journal}{/* &nbsp;{(item.entryTags.doi)?<a href={item.entryTags.doi} rel="noopener noreferrer" target="_blank">[DOI]</a>:<></>}&nbsp;{(item.entryTags.open)?<a href={item.entryTags.oplink} rel="noopener noreferrer" target="_blank">[{item.entryTags.open}]</a>:<></>} */}
+                    {item.entryTags.journal}&nbsp;{(item.entryTags.doi)?<a href={item.entryTags.doi} rel="noopener noreferrer" target="_blank">[DOI]</a>:<></>}&nbsp;{(item.entryTags.open)?<a href={item.entryTags.oplink} rel="noopener noreferrer" target="_blank">[{item.entryTags.open}]</a>:<></>}{/*  */}
                 </Typography>
             </Typography>);
             ++nestedCount;
